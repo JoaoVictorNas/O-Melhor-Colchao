9fb98d782a43dac6631d7f3310ea3abab1ea7878
Joao <joaocre17@gmail.com>
2024-09-02 15:36:08 -0300

    ultimas mudanças do prototipo 1

--- src/Pages/AdminPage/components/AdminHeader/AdminHeader.js
+++ src/Pages/AdminPage/components/AdminHeader/AdminHeader.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './AdminHeader.css';
+import { useNavigate } from 'react-router-dom';
 
-const AdminHeader = ({ onLogout }) => {
+const AdminHeader = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Remove o token de autenticação do localStorage
+        localStorage.removeItem("authToken");
+        // Redireciona para a página de login
+        navigate("/login");
+    };
+
     return (
         <header className="admin-header">
             <h1 className="admin-title">Admin Dashboard</h1>
-            <button onClick={onLogout} className="logout-button">Logout</button>
+            <button onClick={handleLogout} className="logout-button">Logout</button>
         </header>
     );
 };
